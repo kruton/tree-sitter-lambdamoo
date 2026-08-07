675168a3defa30893c7dd2bb0d823ae54009e5aa
Kenny Root <kenny@the-b.org>
2026-08-06 20:12:10 -0700

    Version 0.1.4

--- src/parser.c
+++ src/parser.c
@@ -54403,7 +54403,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_lambdamoo(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 3,
+      .patch_version = 4,
     },
   };
   return &language;
