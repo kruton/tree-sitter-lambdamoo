f2be7f0de6da0f96c8253ceff0034b71ef536ba7
Kenny Root <kenny@the-b.org>
2026-08-07 22:26:03 -0700

    feat: add multiple queries

---
+++ queries/errors.scm
@@ -0,0 +1,11 @@
+(if_statement (MISSING "endif") @missing_endif)
+(for_statement (MISSING "endfor") @missing_endfor)
+(while_statement (MISSING "endwhile") @missing_endwhile)
+(fork_statement (MISSING "endfork") @missing_endfork)
+(try_statement (MISSING "endtry") @missing_endtry)
+(MISSING ")") @missing_paren
+(MISSING "]") @missing_bracket
+(MISSING "}") @missing_brace
+(MISSING "'") @missing_single_quote
+(MISSING ";") @missing_semicolon
+(ERROR) @error
